f60b95260f870cf5f0d51c68cf2462f618c43dc1
Will Smith <minkcv@gmail.com>
2020-01-30 18:46:24 -0700

    fix resize viewport bug

--- components/viewport-component.js
+++ components/viewport-component.js
@@ -1,29 +1,12 @@
 layout.registerComponent( 'viewportComponent', function(container, componentState){
     var renderer = new THREE.WebGLRenderer({antialaias: true});
     var name = 'viewport' + componentState.viewportId;
-    var scale = 10;
     var div = $('<div class="viewport" id="' + name + '"></div>');
-    container.on('show', function() {
-        var width = div.innerWidth();
-        var height = div.innerHeight();
-        renderer.setSize(width, height);
-        var camera = new THREE.OrthographicCamera(
-            -width / scale, width / scale,
-            -height / scale, height / scale,
-            0, 1000
-        );
-        viewports[componentState.viewportId].camera = camera;
+    container.on('tab', function() {
+        updateViewport(div, renderer, componentState.viewportId);
     });
     container.on('resize', function() {
-        var width = div.innerWidth();
-        var height = div.innerHeight();
-        renderer.setSize(width, height);
-        var camera = new THREE.OrthographicCamera(
-            -width / scale, width / scale,
-            -height / scale, height / scale,
-            0, 1000
-        );
-        viewports[componentState.viewportId].camera = camera;
+        updateViewport(div, renderer, componentState.viewportId);
     });
     div.append(renderer.domElement);
     container.getElement().append(div);
@@ -32,3 +15,18 @@ layout.registerComponent( 'viewportComponent', function(container, componentStat
         camera: null
     });
 });
+
+function updateViewport(div, renderer, id) {
+    var scale = 10;
+    var width = div.innerWidth() - 2; // -2 from 1px border
+    var height = div.innerHeight() - 2;
+    if (width < 0 || height < 0)
+        return;
+    renderer.setSize(width, height);
+    var camera = new THREE.OrthographicCamera(
+        -width / scale, width / scale,
+        -height / scale, height / scale,
+        0, 1000
+    );
+    viewports[id].camera = camera;
+}
