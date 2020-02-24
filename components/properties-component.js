c79c26c1af03e4bbc196851543ccb6ff3cc79252
Will Smith <minkcv@gmail.com>
2020-02-23 19:45:50 -0700

    collapsable position rotation scale properties

--- components/properties-component.js
+++ components/properties-component.js
@@ -1,27 +1,33 @@
 layout.registerComponent( 'propertiesComponent', function(container, componentState){
     container.getElement().html(
     `<div class="properties" id="properties">
-        <p>Position</p>
-        <label for='x-pos'>X:</label>
-        <input type='number' name='x-pos' id='x-pos' oninput='changeProperties()'><br>
-        <label for='y-pos'>Y:</label>
-        <input type='number' name='y-pos' id='y-pos' oninput='changeProperties()'><br>
-        <label for='z-pos'>Z:</label>
-        <input type='number' name='z-pos' id='z-pos' oninput='changeProperties()'><br>
-        <p>Rotation (degrees)</p>
-        <label for='x-rot'>X:</label>
-        <input type='number' name='x-rot' id='x-rot' oninput='changeProperties()'><br>
-        <label for='y-rot'>Y:</label>
-        <input type='number' name='y-rot' id='y-rot' oninput='changeProperties()'><br>
-        <label for='z-rot'>Z:</label>
-        <input type='number' name='z-rot' id='z-rot' oninput='changeProperties()'><br>
-        <p>Scale</p>
-        <label for='x-scale'>X:</label>
-        <input type='number' name='x-scale' id='x-scale' oninput='changeProperties()'><br>
-        <label for='y-scale'>Y:</label>
-        <input type='number' name='y-scale' id='y-scale' oninput='changeProperties()'><br>
-        <label for='z-scale'>Z:</label>
-        <input type='number' name='z-scale' id='z-scale' oninput='changeProperties()'><br>
+        <button class='accordion'>Position</button>
+        <div class='panel'>
+            <label for='x-pos'>X:</label>
+            <input type='number' name='x-pos' id='x-pos' oninput='changeProperties()'><br>
+            <label for='y-pos'>Y:</label>
+            <input type='number' name='y-pos' id='y-pos' oninput='changeProperties()'><br>
+            <label for='z-pos'>Z:</label>
+            <input type='number' name='z-pos' id='z-pos' oninput='changeProperties()'><br>
+        </div>
+        <button class='accordion'>Rotation</button>
+        <div class='panel'>
+            <label for='x-rot'>X:</label>
+            <input type='number' name='x-rot' id='x-rot' oninput='changeProperties()'><br>
+            <label for='y-rot'>Y:</label>
+            <input type='number' name='y-rot' id='y-rot' oninput='changeProperties()'><br>
+            <label for='z-rot'>Z:</label>
+            <input type='number' name='z-rot' id='z-rot' oninput='changeProperties()'><br>
+        </div>
+        <button class='accordion'>Scale</button>
+        <div class='panel'>
+            <label for='x-scale'>X:</label>
+            <input type='number' name='x-scale' id='x-scale' oninput='changeProperties()'><br>
+            <label for='y-scale'>Y:</label>
+            <input type='number' name='y-scale' id='y-scale' oninput='changeProperties()'><br>
+            <label for='z-scale'>Z:</label>
+            <input type='number' name='z-scale' id='z-scale' oninput='changeProperties()'><br>
+        </div>
         <div id='camera-properties'>
             <hr>
             <p>Camera</p>
